1b5297b81f7b7f00ff21d9215c6512c504fa724d
Zhenhua yu <zyu0628@gmail.com>
2020-10-10 19:16:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
   var array =[1]
-  array = ["element", ...array]
+  array.unshift("element")
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   const array = [1]
