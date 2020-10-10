3ad27bd46fd796a4ed173ea97e85ad1830873829
Zhenhua yu <zyu0628@gmail.com>
2020-10-10 18:48:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["snikcer","hundred grand","kitkat","skittles"] 
+
+function addElementToBeginningOfArray(array, element){
+  const array =[]
+  array.unshift(elementN)
+  console.log(array);
+  return array;
+}
+var destructivelyAddElementToBeginningOfArray ()
