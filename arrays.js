b89b42620eec4517a6439158641be8f41ad5364e
Zhenhua yu <zyu0628@gmail.com>
2020-10-10 19:07:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, 'element'){
+function addElementToBeginningOfArray(array, "element"){
   var array =[1]
   array = ["element", ...array]
   return array
